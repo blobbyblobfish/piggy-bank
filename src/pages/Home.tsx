72de090306570d099a30691c2e7479af8848b7df
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-08 18:51:37 -0400

    adds home information

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
@@ -8,16 +8,32 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Welcome to the Piggy Bank</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Welcome to the Piggy Bank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        {/* <ExploreContainer /> */}
+        <IonGrid>
+          <IonRow>
+            <IonCol size-md="6">
+                <IonCard>
+                  <IonCardContent>
+                    <IonItem>
+                      <IonLabel position="floating"> Sign up </IonLabel>
+                    </IonItem>
+                    <IonItem>
+                      <IonLabel position="floating"> Log in </IonLabel>
+                    </IonItem>
+                  </IonCardContent>
+                </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
