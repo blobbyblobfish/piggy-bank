df5ec2f8810fa3366f3fe150db639504e7a7e96b
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-09 00:41:57 -0400

    removes form-control

--- src/components/login.js
+++ src/components/login.js
@@ -1,24 +1,27 @@
 import React, { Component } from "react";
-import { Form, Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 export default class Login extends Component {
     render() {
         return (
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+            <form>
+                <h3>Log In</h3>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" id="log-in-email" className="form-control" placeholder="Enter email" />
+                </div>
 
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" id="log-in-password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </form>
         );
     }
 }
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,35 +1,31 @@
 import React, { Component } from "react";
-import { Form, Button } from 'react-bootstrap';
 
 export default class SignUp extends Component {
     render() {
         return (
-            <Form>
-                 <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="name" placeholder="Enter your name" />
-                </Form.Group>
+            <form>
+                <h3>Sign Up</h3>
 
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+                <div className="form-group">
+                    <label>Name</label>
+                    <input type="text" id="sign-up-name" className="form-control" placeholder="Enter your name please" />
+                </div>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" id="sign-up-email" className="form-control" placeholder="Enter email" />
+                </div>
 
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" id="sign-up-password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
                 <p className="forgot-password text-right">
-                    Already registered <a href="#">Log in?</a>
+                    Already registered <a href="#">sign in?</a>
                 </p>
-            </Form>
+            </form>
         );
     }
 }
