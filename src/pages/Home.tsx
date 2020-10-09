5f16d0536e2474d203c3f307998916dd752b14fb
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-08 20:40:41 -0400

    adds router and navbar to login and signup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 //import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+// import ExploreContainer from '../components/ExploreContainer';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './Home.css';
 
@@ -10,17 +10,15 @@ import SignUp from '../components/signup';
 const Home: React.FC = () => {
   return (
     <Router>
-    <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
-        <h1> Welcome to the Piggy-Bank</h1>
-          {/* <Link className="navbar-brand" to={"/sign-in"}> Welcome to the Piggy-Bank</Link> */}
+        <h1> Welcome to the Piggy-Bank </h1>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+            <ul className="navbar-nav list-group">
+              <li className="nav-item list-group-item">
+                <Link className="nav-link" to={"/log-in"}>Login</Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item list-group-item">
                 <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
               </li>
             </ul>
@@ -32,12 +30,12 @@ const Home: React.FC = () => {
         <div className="auth-inner">
           <Switch>
             <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
+            <Route path="/log-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
           </Switch>
         </div>
       </div>
-    </div></Router>
+    </Router>
     
   );
 };
