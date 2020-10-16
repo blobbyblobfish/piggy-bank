c6278719f5d66bc80befd159f9496127a40f623c
SincerelyBrittany <sincerelybr@gmail.com>
2020-10-16 00:48:52 -0400

    updates the css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,11 +12,13 @@ export default class Home extends Component {
   render() {
     return (
       <div className = "home-div">
+        <div className = "home-container">
         <img src={logo} alt="Logo" />
             <div className = "login-signup-div">
               <NavLink className = "loginButton" to="/login">Login</NavLink>
               <NavLink className = "signUpButton" to="/signup">Sign Up</NavLink>
           </div>
+         </div>
       </div>
     )
   }
