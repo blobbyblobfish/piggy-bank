86671de25731983543766463b1745bda999f1ee4
Alice Wang <aywang6@gmail.com>
2020-10-16 11:20:13 -0400

    #11 Set up quiz routing

--- src/components/Lesson.js
+++ src/components/Lesson.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 import './Lesson.css';
 
-export default function Lesson(props) {
+function Lesson(props) {
 
     const lesson = props.lesson
 
-    console.log("HI THIS IS A TEST")
-    console.log(lesson)
+    function doneWatching() {
+        props.history.push(`/lesson/${lesson.id}/quiz`) //push to quiz page
+    }
 
     return (
         <div className="lesson-container">
@@ -16,9 +18,37 @@ export default function Lesson(props) {
                 gyroscope; picture-in-picture"
                 allowfullscreen>
             </iframe>
-            <button className="done-btn">Done Watching</button> 
-            {/* If done watching is clicked, navigate to quiz */}
+            <button className="done-btn" onClick={doneWatching}>Done Watching</button> 
            
         </div>
     )
 }
+
+export default withRouter(Lesson)
+
+// export default class Lesson extends React.component {
+
+//     render() {
+//         const lesson = this.props.lesson
+
+//         console.log(this.props.history)
+        
+//         function doneWatching() {
+//             this.props.history.push(`/lesson/${lesson.id}/quiz`)
+//         }
+
+//     return (
+//         <div className="lesson-container">
+//             <h1>{lesson.name}</h1>
+//             <iframe width="560" height="315" src={lesson.video_url} title={lesson.title}
+//                 frameborder="0" className="lesson-video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+//                 gyroscope; picture-in-picture"
+//                 allowfullscreen>
+//             </iframe>
+//             {/* If done watching is clicked, navigate to quiz */}
+//             <button className="done-btn" onClick={doneWatching}>Done Watching</button> 
+           
+//         </div>
+//     )
+//     }
+// }
